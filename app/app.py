114d2d0e3adb887424d6edd335e81ee7438e11b0
NarvekarHritika <hritikavn72@gmail.com>
2025-12-24 17:53:51 +0530

    fixing bug: getFeed bug

--- app/app.py
+++ app/app.py
@@ -151,6 +151,10 @@ async def get_feed(
     result = await session.execute(select(Post).order_by(Post.created_at.desc()))
     posts = [row[0] for row in result.all()]
 
+    result = await session.execute(select(User))
+    users = [row[0] for row in result.all()]
+    user_dict = {u.id: u.email for u in users}
+
     # Format the posts data for the response.
     posts_data = []
     for post in posts:
@@ -163,10 +167,9 @@ async def get_feed(
                 "file_name": post.file_name,
                 "created_at": post.created_at.isoformat(),
                 "is_owner": post.user_id == user.id,
-                "email": user.email,
+                "email": user_dict.get(post.user_id, "Unknown"),
             }
         )
-
     return {"posts": posts_data}
 
 
